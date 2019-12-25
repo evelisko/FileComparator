7c4d1fbe821f148223e6c5227569efc9dd051f48
evelisko <37504765+evelisko@users.noreply.github.com>
2019-12-25 16:30:17 +0400

    probe Repository Initialize

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FileComparator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dGvFileList = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dGvFileList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 82);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Запустить сравнение. ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 38);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(531, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 38);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(513, 22);
+            this.textBox1.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(513, 22);
+            this.textBox2.TabIndex = 28;
+            // 
+            // dGvFileList
+            // 
+            this.dGvFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dGvFileList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGvFileList.Location = new System.Drawing.Point(12, 130);
+            this.dGvFileList.Name = "dGvFileList";
+            this.dGvFileList.RowHeadersWidth = 51;
+            this.dGvFileList.RowTemplate.Height = 24;
+            this.dGvFileList.Size = new System.Drawing.Size(747, 490);
+            this.dGvFileList.TabIndex = 29;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 626);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(747, 13);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 647);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dGvFileList);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Поиск различных файлов. ";
+            ((System.ComponentModel.ISupportInitialize)(this.dGvFileList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dGvFileList;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileComparator
+{
+    public struct FileAtributes 
+    {
+     public string fileName; 
+     public int    fileSize;  
+    }
+
+
+    public partial class Form1 : Form
+    {
+
+        List<FileAtributes> FilesA;
+        List<FileAtributes> FilesB;
+        string filesPathA;
+        string filesPathB; 
+     // Создаем список файлов. 
+     // Нужно создать тип файла
+     // определяе различия 
+
+      public Form1()
+        {
+          InitializeComponent();
+
+            dGvFileList.AllowUserToAddRows = false;
+            dGvFileList.AllowUserToDeleteRows = false;
+            dGvFileList.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dGvFileList.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dGvFileList.Columns.Add("Index", "Index");
+            dGvFileList.Columns.Add("FileName", "FileName");
+            dGvFileList.Columns.Add("Size", "Size");
+
+            dGvFileList.Columns[0].Width = 90;
+            dGvFileList.Columns[1].Width = 90;
+            dGvFileList.Columns[2].Width = dGvFileList.Width - 180 - dGvFileList.RowHeadersWidth;
+            FilesA = new List<FileAtributes>();
+            FilesB = new List<FileAtributes>();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          FolderBrowserDialog fbDialogFileA = new FolderBrowserDialog();
+            if (fbDialogFileA.ShowDialog() == DialogResult.OK)
+            {
+                filesPathA = fbDialogFileA.SelectedPath;
+                textBox1.Text = filesPathA; 
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+     //       string nameFileA;
+     //       string sizeFileA;
+            FileAtributes fA = new FileAtributes();
+            // запускаем поиск файлов расположенных в текущем каталоге. с учетом подкаталогов. 
+            label1.Text = "Индексирование элементов массива А";
+            string[] allfilesA = Directory.GetFiles(filesPathA, "*.*", SearchOption.AllDirectories);
+
+            //возникает ошибка при индексировании. 
+            // Нужно проводить индексирование файлов в каждома каталоге
+            // Добавлять в каталог файл сразу при получении его имени.
+            // При ошибке доступа к каталогу выводить соответствующее исключение. 
+            // выводить разрешение файла
+
+            for (int indexA = 0; indexA < allfilesA.Length; indexA++)
+            {
+                fA.fileName = Path.GetFileName(allfilesA[indexA]);
+                // добавляем файлы в массив.
+                string strEx = Path.GetExtension(allfilesA[indexA]);
+               if (strEx != ".cs")
+                FilesA.Add(fA);
+                progressBar1.Value = (int)((indexA / allfilesA.Length) * 100); 
+            }
+            label1.Text = "Индексирование элементов массива B";
+            string[] allfilesB = Directory.GetFiles(filesPathB, "*.*", SearchOption.AllDirectories);
+
+            for (int indexB = 0; indexB < allfilesB.Length; indexB++)// foreach (string filename in allfilesB)
+            {
+                fA.fileName = Path.GetFileName(allfilesB[indexB]);
+                // добавляем файлы в массив.
+                if (Path.GetExtension(allfilesB[indexB]) != ".cs")
+                    FilesB.Add(fA);
+                progressBar1.Value = (int)((indexB / allfilesB.Length) * 100);
+            }
+
+            // Файлы проиндексированы
+            // Проводим сравнение файлов. 
+
+            // DirectoryInfo dir = new DirectoryInfo(@"D:\Temp");
+            // foreach (var item in dir.GetDirectories())
+            // {
+            //   Console.WriteLine(item.Name);
+            // }
+            //Console.ReadLine();
+
+            label1.Text = "Сравнение элементов массива"; 
+            int index = 0;
+            int matches;// Можно будет попробовать реализовать перебор как в Java. 
+            for (int indexB = 0; indexB < FilesB.Count;indexB++)
+            {
+                matches = 0;
+                //при проходе файл накапливает массу. 
+                for (int indexA =0; indexA< FilesA.Count;indexA++)
+                {
+                    if (FilesA[indexA].fileName == FilesB[indexB].fileName)
+                    {
+                      matches++;
+                    }
+          
+                } 
+
+                if (matches == 0)
+                    {
+                      index++;
+                      dGvFileList.Rows.Add(1);
+                      dGvFileList[0, dGvFileList.Rows.Count - 1].Value = index.ToString();
+                      dGvFileList[1, dGvFileList.Rows.Count - 1].Value = FilesB[indexB].fileName;
+                    }
+                progressBar1.Value = (int)((indexB / FilesB.Count) * 100);
+            }
+            label1.Text = "Вывод результата";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbDialogFileB = new FolderBrowserDialog();
+            if (fbDialogFileB.ShowDialog() == DialogResult.OK)
+            {
+                filesPathB = fbDialogFileB.SelectedPath;
+                textBox2.Text = filesPathB;
+            }
+
+        }
+    }
+}
